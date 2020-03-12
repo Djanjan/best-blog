93db68c69cd1c57b2908348ecb8bb8f5c025897d
Djanjan <34454544+Djanjan@users.noreply.github.com>
2020-03-12 14:04:18 +0300

    fix padding appCardSimple

--- src/components/AppCardSimple.vue
+++ src/components/AppCardSimple.vue
@@ -1,54 +1,73 @@
 <template>
-    <v-card-text>
-    <div class="mb-1 headline"  :style="'white-space: normal; margin-left:'+(20+circleSize)+'px'">Landscape design</div>
-
-    <v-card
-      :class="customClass">
-
-    <div style="height: 0px;">
-      <template v-if="!circleMin">
-        <v-avatar :color="$vuetify.theme.dark ? '#424242':'white'" :size="circleSize+10" :style="'top:' + -((circleSize/2)+6) + 'px ; left:15px'">
-        <v-avatar :color="circleColor" :size="circleSize">
-          <v-icon v-if="!isAvatar" color="white" :size="circleSize/2">{{icon}}</v-icon>
-          <img v-else :src="this.avatar"/>
-        </v-avatar>
-        </v-avatar>
-      </template>
-
-      <template v-else>
-        <v-avatar :color="circleColor" :size="circleSize" :style="'top:' + -((circleSize/2)) + 'px ; left:15px'">
-          <v-icon v-if="!isAvatar" color="white" :size="circleSize/2">{{icon}}</v-icon>
-          <img v-else :src="this.avatar"/>
-        </v-avatar>
-      </template>
+  <v-card-text style="padding: 0px">
+    <div
+      class="mb-1 headline"
+      :style="'white-space: normal; margin-left:' + (20 + circleSize) + 'px'"
+    >
+      Landscape design
     </div>
 
-    <v-list-item two-line class="text-right" style="padding-left: 110px;">
-                <v-list-item-content>
-                    <v-list-item-title class="subtitle-1 mb-1"  style="white-space: normal;">Landscape design</v-list-item-title>
-                    <v-list-item-subtitle class="subtitle-2" style="white-space: normal;">Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-                </v-list-item-content>
-    </v-list-item>
+    <v-card :class="customClass">
+      <div style="height: 0px;">
+        <template v-if="!circleMin">
+          <v-avatar
+            :color="$vuetify.theme.dark ? '#424242' : 'white'"
+            :size="circleSize + 10"
+            :style="'top:' + -(circleSize / 2 + 6) + 'px ; left:15px'"
+          >
+            <v-avatar :color="circleColor" :size="circleSize">
+              <v-icon v-if="!isAvatar" color="white" :size="circleSize / 2">{{
+                icon
+              }}</v-icon>
+              <img v-else :src="this.avatar" />
+            </v-avatar>
+          </v-avatar>
+        </template>
 
-     <v-divider class="mx-4"></v-divider>
+        <template v-else>
+          <v-avatar
+            :color="circleColor"
+            :size="circleSize"
+            :style="'top:' + -(circleSize / 2) + 'px ; left:15px'"
+          >
+            <v-icon v-if="!isAvatar" color="white" :size="circleSize / 2">{{
+              icon
+            }}</v-icon>
+            <img v-else :src="this.avatar" />
+          </v-avatar>
+        </template>
+      </div>
 
-    <v-card-text class="px-6">
-                <v-row align="center">
-                  <div class="font-weight-light caption">
-                    <v-icon medium>mdi-clock-outline</v-icon>
-                    Last Update
-                  </div>
-                  <v-spacer></v-spacer>
+      <v-list-item two-line class="text-right" style="padding-left: 110px;">
+        <v-list-item-content>
+          <v-list-item-title
+            class="subtitle-1 mb-1"
+            style="white-space: normal;"
+            >Landscape design</v-list-item-title
+          >
+          <v-list-item-subtitle class="subtitle-2" style="white-space: normal;"
+            >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
+          >
+        </v-list-item-content>
+      </v-list-item>
 
-                  <v-btn icon small>
-                    <v-icon class="">mdi-bookmark</v-icon>
-                  </v-btn>
+      <v-divider class="mx-4"></v-divider>
 
-                </v-row>
-    </v-card-text>
+      <v-card-text class="px-6">
+        <v-row align="center">
+          <div class="font-weight-light caption">
+            <v-icon medium>mdi-clock-outline</v-icon>
+            Last Update
+          </div>
+          <v-spacer></v-spacer>
 
+          <v-btn icon small>
+            <v-icon class="">mdi-bookmark</v-icon>
+          </v-btn>
+        </v-row>
+      </v-card-text>
     </v-card>
-    </v-card-text>
+  </v-card-text>
 </template>
 
 <script>
@@ -84,7 +103,6 @@ export default {
       default: ''
     }
   },
-  data: () => ({
-  })
+  data: () => ({})
 }
 </script>
