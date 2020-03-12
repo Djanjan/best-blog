c449c48f1c8c4c25cf038c2e4f9d1ffd7861d9be
Djanjan <34454544+Djanjan@users.noreply.github.com>
2020-03-12 15:39:31 +0300

    Updates

---
+++ js/chunk-vendors~360570dd.4fe54f14.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~360570dd"],{"95ed":function(n,d,o){}}]);
+//# sourceMappingURL=chunk-vendors~360570dd.4fe54f14.js.map
--- js/chunk-vendors~8f64ed1d.3bd26fd5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~8f64ed1d"],{"95ed":function(n,o,c){},ec29:function(n,o,c){}}]);
-//# sourceMappingURL=chunk-vendors~8f64ed1d.3bd26fd5.js.map
